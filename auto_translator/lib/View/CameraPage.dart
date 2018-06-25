eacd8e4f06b838567534eb2c93394cf1ea68fc2b
Kim HyunWoo <stories2@naver.com>
2018-06-26 05:25:18 +0900

    CameraPage : request camera permission
    develop page : send firebase event and login with google id

--- auto_translator/lib/View/CameraPage.dart
+++ auto_translator/lib/View/CameraPage.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
 class CameraPage extends StatefulWidget {
 
@@ -10,11 +14,157 @@ class CameraPage extends StatefulWidget {
 
 class CameraPageState extends State<CameraPage> {
 
+  List<CameraDescription> cameras;
+  CameraController cameraController;
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: null,
+      body: new Column(
+        children: <Widget>[
+          new Expanded(
+              child: new Container(
+                child: new Padding(
+                    padding: EdgeInsets.all(1.0),
+                    child: CameraPreviewWidget(),
+                ),
+                decoration: new BoxDecoration(
+                  color: Colors.black,
+                  border: new Border.all(
+                    color: Colors.red,
+                    width: 3.0
+                  )
+                ),
+              )
+          ),
+          CameraControlWidget(),
+          new Padding(
+              padding: new EdgeInsets.all(5.0),
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  ThumbnailPreviewWidget()
+                ],
+              ),
+          )
+        ],
+      ),
       backgroundColor: Colors.orange,
     );
+  }
+
+  Widget CameraPreviewWidget() {
+    return new Center(
+      child: new Text(
+        "Tap a camera",
+        style: new TextStyle(
+            color: Colors.white,
+            fontSize: 24.0,
+            fontWeight: FontWeight.w900
+        ),
+      ),
+    );
+  }
+
+  Widget CameraControlWidget() {
+    return new Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        new IconButton(
+            icon: new Icon(Icons.camera_alt),
+            color: Colors.white,
+            onPressed: TestOnClick
+        )
+      ],
+    );
+  }
+
+  Widget ThumbnailPreviewWidget() {
+    return new Expanded(
+        child: new Align(
+          alignment: Alignment.centerLeft,
+          child: new SizedBox(
+            child: new Container(
+              decoration: new BoxDecoration(
+                border: new Border.all(
+                  color: Colors.green
+                )
+              ),
+            ),
+            width: 64.0,
+            height: 64.0,
+          ),
+        )
+    );
+  }
+
+  Future<Null> TestOnClick() {
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    InitCamera();
+  }
+
+  void InitCamera() async {
+    try {
+      cameras = await availableCameras();
+      print("camera init ok");
+
+//      for(CameraDescription camDescription in cameras) {
+//        print("des");
+//      }
+
+      ReadyForUseCam(cameras[0]);
+    }
+    catch(except) {
+      print("error while init cam: " + except.code + " des: " + except.description);
+    }
+  }
+
+  void ReadyForUseCam(CameraDescription cameraDescription) async {
+    if(cameraController != null) {
+      cameraController.dispose();
+    }
+    cameraController = new CameraController(
+        cameraDescription,
+        ResolutionPreset.high
+    );
+    
+    cameraController.addListener(() {
+      if(mounted) {
+        this.setState(() {
+
+        });
+      }
+      if(cameraController.value.hasError) {
+        print("cam error: " + cameraController.value.errorDescription);
+      }
+    });
+
+    try {
+      await cameraController.initialize();
+      print("cam initialize ok");
+    }
+    catch(except) {
+      print("cam initialize failed: " + except);
+    }
+
+    if(mounted) {
+      this.setState(() {
+
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    if(cameraController != null) {
+      cameraController.dispose();
+    }
+    super.dispose();
   }
 }
--- auto_translator/lib/View/DevelopPage.dart
+++ auto_translator/lib/View/DevelopPage.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-//import 'package:firebase_analytics/firebase_analytics.dart';
-//import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class DevelopPage extends StatefulWidget {
 
@@ -14,11 +16,85 @@ class DevelopPage extends StatefulWidget {
 
 class DevelopPageState extends State<DevelopPage> {
 
+  FirebaseAnalytics analytics;
+  FirebaseAnalyticsObserver analyticsObserver;
+  FirebaseAuth firebaseAuth;
+  GoogleSignIn googleSignIn;
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: null,
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new MaterialButton(
+              onPressed: SendTestAnalyticsEvent,
+              color: Colors.redAccent,
+              child: new Text(
+                  "Send test analytics event"
+              ),
+            ),
+            new Padding(
+                padding: new EdgeInsets.only(
+                  bottom: 10.0
+                )
+            ),
+
+            new MaterialButton(
+                onPressed: SignInGoogleAccount,
+                color: Colors.orange,
+                child: new Text(
+                  "Test google auth"
+                ),
+            )
+          ],
+        ),
+      ),
       backgroundColor: Colors.blueAccent,
     );
+  }
+
+  Future<Null> SignInGoogleAccount() async {
+    GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleSignInAccount.authentication;
+    FirebaseUser firebaseUser = await firebaseAuth.signInWithGoogle(
+        idToken: googleAuth.idToken,
+        accessToken: googleAuth.accessToken
+    );
+
+    print("sign in account: " + firebaseUser.email);
+
+    FirebaseUser currentUser = await firebaseAuth.currentUser();
+    assert(currentUser.uid == firebaseUser.uid);
+
+  }
+
+  Future<Null> SendTestAnalyticsEvent() async {
+    await analytics.logEvent(
+        name: 'test_event',
+        parameters: <String, dynamic> {
+          'string': 'string',
+          'int': 42,
+          'long': 123456789,
+          'double': 43.0,
+          'bool': true
+        }
+    );
+    print("event sent successfully");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    analytics = new FirebaseAnalytics();
+    analyticsObserver = new FirebaseAnalyticsObserver(analytics: analytics);
+    firebaseAuth = FirebaseAuth.instance;
+    googleSignIn = new GoogleSignIn();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
